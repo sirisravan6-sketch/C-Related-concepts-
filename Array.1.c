8d6e46fce406c5d1a918bf07ab6f88009f468983
T.SRAVAN <sirisravan6@gmail.com>
2026-09-23 18:47:16 +0530

    Add files via upload

---
+++ Array.1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+     
+int main() { 
+    int marks[]={10,20,30,40,50};
+ for(int i=0;i<5;i++){
+    printf("The value of %d element of the array is %d\n",i,marks[i]);
+}
+return 0;
+}
